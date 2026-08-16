2e0eaf971cb8822abb9f51acbf284dd4f948890d
rajekroman <108610159+rajekroman@users.noreply.github.com>
2026-08-16 09:41:40 +0200

    A7: export A4 analytics oracle

--- src/analyzazprav/qa/__init__.py
+++ src/analyzazprav/qa/__init__.py
@@ -1,3 +1,4 @@
+from .a4 import validate_a4_metrics
 from .reconciliation import validate_staging_bundle
 from .staging import STATUS_FAIL, STATUS_PASS, STATUS_WARNING, validate_staging_dir
 from .vertical import canonical_fingerprint, validate_vertical_pipeline
@@ -9,5 +10,6 @@ __all__ = [
     "validate_staging_dir",
     "validate_staging_bundle",
     "validate_vertical_pipeline",
+    "validate_a4_metrics",
     "canonical_fingerprint",
 ]
