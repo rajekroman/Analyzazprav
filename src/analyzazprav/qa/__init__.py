9a831b507c8840c46adbddcd5b51f243ff8d80ea
rajekroman <108610159+rajekroman@users.noreply.github.com>
2026-08-16 09:48:23 +0200

    A7: route A4 oracle through resolved participant contract

--- src/analyzazprav/qa/__init__.py
+++ src/analyzazprav/qa/__init__.py
@@ -1,4 +1,4 @@
-from .a4 import validate_a4_metrics
+from .a4_current import validate_a4_metrics
 from .reconciliation import validate_staging_bundle
 from .staging import STATUS_FAIL, STATUS_PASS, STATUS_WARNING, validate_staging_dir
 from .vertical import canonical_fingerprint, validate_vertical_pipeline
