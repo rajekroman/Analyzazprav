31b9b108ca66d6db3363804baa574869f469e24c
rajekroman <108610159+rajekroman@users.noreply.github.com>
2026-08-16 09:28:21 +0200

    A7: export reconciliation-aware staging bundle validator

--- src/analyzazprav/qa/__init__.py
+++ src/analyzazprav/qa/__init__.py
@@ -1,3 +1,4 @@
+from .reconciliation import validate_staging_bundle
 from .staging import STATUS_FAIL, STATUS_PASS, STATUS_WARNING, validate_staging_dir
 from .vertical import canonical_fingerprint, validate_vertical_pipeline
 
@@ -6,6 +7,7 @@ __all__ = [
     "STATUS_WARNING",
     "STATUS_FAIL",
     "validate_staging_dir",
+    "validate_staging_bundle",
     "validate_vertical_pipeline",
     "canonical_fingerprint",
 ]
