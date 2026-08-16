60a765bf3e2eafd2f320e4a54e5621cf7af6e62a
rajekroman <108610159+rajekroman@users.noreply.github.com>
2026-08-16 09:43:50 +0200

    A7: expose downstream validation API

--- src/analyzazprav/qa/__init__.py
+++ src/analyzazprav/qa/__init__.py
@@ -1,3 +1,13 @@
+from .downstream import (
+    VERDICT_INVALID,
+    VERDICT_NEEDS_REVIEW,
+    VERDICT_VALID,
+    aggregate_release_verdict,
+    validate_a4_result,
+    validate_a5_result,
+    validate_a6_contract,
+    validate_a6_renderer_source,
+)
 from .reconciliation import validate_staging_bundle
 from .staging import STATUS_FAIL, STATUS_PASS, STATUS_WARNING, validate_staging_dir
 from .vertical import canonical_fingerprint, validate_vertical_pipeline
@@ -6,8 +16,16 @@ __all__ = [
     "STATUS_PASS",
     "STATUS_WARNING",
     "STATUS_FAIL",
+    "VERDICT_VALID",
+    "VERDICT_INVALID",
+    "VERDICT_NEEDS_REVIEW",
     "validate_staging_dir",
     "validate_staging_bundle",
     "validate_vertical_pipeline",
     "canonical_fingerprint",
+    "validate_a4_result",
+    "validate_a5_result",
+    "validate_a6_contract",
+    "validate_a6_renderer_source",
+    "aggregate_release_verdict",
 ]
